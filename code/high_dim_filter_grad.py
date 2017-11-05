9eceaf87e6b6aee29b96e1079e9d2f4842f56544
Chen Liu <liuchenwustl@gmail.com>
2017-11-05 14:45:47 -0600

    minor changes

---
+++ code/high_dim_filter_grad.py
@@ -0,0 +1,52 @@
+"""
+MIT License
+
+Copyright (c) 2017 Sadeep Jayasumana
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+"""
+
+import tensorflow as tf
+from tensorflow.python.framework import ops
+custom_module = tf.load_op_library('./cpp/high_dim_filter.so')
+
+
+@ops.RegisterGradient("HighDimFilter")
+def _high_dim_filter_grad(op, grad):
+    """ Gradients for the HighDimFilter op. We only need to calculate the gradients
+    w.r.t. the first input (unaries) as we never need to backprop errors to the
+    second input (RGB values of the image).
+
+    Args:
+    op: The `high_dim_filter` operation that we are differentiating.
+    grad: Gradients with respect to the output of the `high_dim_filter` op.
+
+    Returns:
+    Gradients with respect to the input of `high_dim_filter`.
+    """
+
+    rgb = op.inputs[1]
+    grad_vals = custom_module.high_dim_filter(grad, rgb,
+                                              bilateral=op.get_attr("bilateral"),
+                                              theta_alpha=op.get_attr("theta_alpha"),
+                                              theta_beta=op.get_attr("theta_beta"),
+                                              theta_gamma=op.get_attr("theta_gamma"),
+                                              backwards=True)
+
+    return [grad_vals, tf.zeros_like(rgb)]
